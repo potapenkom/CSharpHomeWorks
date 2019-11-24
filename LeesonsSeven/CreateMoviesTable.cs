e78b6d1ff647518844a9daffcd3f037487554f7e
Mikhail Potapenko <it.potapenkom@gmail.com>
2019-11-24 10:18:01 +0200

    added scrir create table

---
+++ LeesonsSeven/CreateMoviesTable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace LeesonsSeven
+{
+    public class CreateMoviesTable
+    {
+        public static void createMoviesTable()
+        {
+            string queryString = @"CREATE TABLE dbo.[Movies]
+                           (
+                               [ID] [int] NOT NULL,  
+                               [Name] [nvarchar](50) NULL,
+                               [Genre] [nvarchar](50) NULL,
+                               [Year] [int] NOT NULL,   
+                           ) on [PRIMARY];";
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = ConfigurationManager.ConnectionStrings["Cinema"].ToString();
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+    }
+}
